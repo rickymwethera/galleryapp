c864e9373ef78d1502647b32ffb8bdfd693809f0
rickymwethera <rickymwethera@gmail.com>
2020-10-15 08:37:12 +0300

    modified urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^galleryapp/',include('galleryapp.urls'))
+    url(r'',include('galleryapp.urls'))
 ]
